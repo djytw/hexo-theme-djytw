079ec6c114c75e564b398ad1a38a430ba4523fef
djytw <djytw@live.com>
2019-11-12 23:06:19 +0100

    daily modify

--- source/js/doc.js
+++ source/js/doc.js
@@ -1,11 +1,25 @@
 
 
 function ui_scrollfire_card(){
-	var elems = document.querySelectorAll('#mainc>.card:not(.show)');
+	var elems = document.querySelectorAll('#mainc>.card:not(.show),#sidebar>.card:not(.show)');
 	elems.forEach(function(t){
-		if(t.getBoundingClientRect().top+100<window.innerHeight)
+		if(t.getBoundingClientRect().top+100<window.innerHeight){
 			t.classList.add('show');
+		}
 	})
+}
+function ui_netease_init(){
+	if(window.innerWidth<993)return;
+	var fr = document.getElementById("neteasemusic");
+	if(fr.innerText == "")
+	fr.innerHTML='<div><iframe frameborder="no" height="450" src="https://music.163.com/outchain/player?type=0&id=2051063875&auto=0&height=430" onload="ui_netease_loaded()">\
+			</iframe><a href="https://music.163.com/user/61422243">查看歌单</a></div><div>播放器正在加载....</div>';
+}
+function ui_netease_loaded(){
+	var fr = document.getElementById("neteasemusic");
+	fr.removeChild(fr.children[1]);
+	fr.children[0].style.visibility="visible";
+	fr.style.background="none";
 }
 function ui_materialboxed_init(){
 	document.querySelectorAll('.materialboxed').forEach(function(elem){
@@ -89,32 +103,17 @@ var loading='<div class="preloader-wrapper big active">\
 function util_github_eventrender(e){
 	var gitevent = document.querySelector("#git-event");
 	gitevent.innerHTML="";
-	var msg = "";
-	var p = 4;
-	for (var i = 0; i < e.length; i++) {
-		if (e[i].type == "PushEvent") {
-			var time = e[i].created_at.split('T')[0];
-			time = time.split('-');
-			time = time[1] + "-" + time[2];
-			var branch = e[i].payload.ref.split("/")[2];
-			if (msg == e[i].payload.commits[0].message) {
-				msg = "";
-				p++;
-				continue;
-			} else msg = e[i].payload.commits[0].message;
-			var s;
-			s = "<div>" + time + "<a href=\"" + e[i].repo.url + "\">" + e[i].repo.name + "</a>#<a href=\"" + e[i].repo.url + "/tree/" + branch + "\">" + branch + "</a></div>";
-			s += "<div><a href=\"" + e[i].payload.commits[0].url + "\">" + e[i].payload.commits[0].sha.slice(0, 7) + "</a>" + msg + "</div>";
-			gitevent.innerHTML+=s;
-			if (i >= p) break;
-		} else {
-			p++;
-		}
+	for (var t of e) {
+		var s;
+		s = "<div>" + t.time + "<a href=\"" + t.url + "\">" + t.name + "</a>#<a href=\"" + t.url + "/tree/" + t.branch + "\">" + t.branch + "</a></div>";
+		s += "<div><a href=\"" + t.curl + "\">" + t.csha + "</a>" + t.msg + "</div>";
+		gitevent.innerHTML+=s;
 	}
 }
 function util_github_getevent() {
 	var gitevent = document.querySelector("#git-event");
 	var githubevent = Cookies.get("githubevent");
+	var msg = "";
 	if (githubevent == undefined) {
 		gitevent.innerHTML=loading;
 		var oReq = new XMLHttpRequest();
@@ -122,8 +121,28 @@ function util_github_getevent() {
 		oReq.responseType = "json";
 		oReq.onload =  function (err) {
 			var e = oReq.response;
-			Cookies.set("githubevent",JSON.stringify(e));
-			util_github_eventrender(e);
+			var ret = []
+			var p = 4;
+			for (var i = 0; i < e.length; i++) {
+				if (e[i].type == "PushEvent") {
+					var time = e[i].created_at.split('T')[0];
+					time = time.split('-');
+					time = time[1] + "-" + time[2];
+					var branch = e[i].payload.ref.split("/")[2];
+					if (msg == e[i].payload.commits[0].message) {
+						msg = "";
+						p++;
+						continue;
+					} else msg = e[i].payload.commits[0].message;
+					ret.push({time:time,url:e[i].repo.url,name:e[i].repo.name,branch:branch,curl:e[i].payload.commits[0].url,csha:e[i].payload.commits[0].sha.slice(0, 7),msg:msg});
+					if (i >= p) break;
+				} else {
+					p++;
+				}
+			}
+			var exp = new Date(new Date().getTime() + 5 * 60 * 1000);
+			Cookies.set("githubevent",JSON.stringify(ret),{expires: exp});
+			util_github_eventrender(ret);
 		}
 		oReq.send(null);
 	} else {
@@ -134,13 +153,12 @@ function util_github_getevent() {
 function util_github_reposrender(e){
 	var gitrepos= document.querySelector("#git-repos");
 	gitrepos.innerHTML="";
-	for (var i = 0; i < e.length; i++) {
-		var s = "<div><a href=\"" + e[i].html_url + "\">" + e[i].name + "</a></div><div>";
-		if (e[i].description == null) s += "没有简介...";
-		else s += e[i].description;
+	for (var t of e) {
+		var s = "<div><a href=\"" + t.url + "\">" + t.name + "</a></div><div>";
+		if (t.des == null) s += "没有简介...";
+		else s += t.des;
 		s += "</div>";
 		gitrepos.innerHTML+=s;
-		if (i >= 4) break;
 	}
 	gitrepos.innerHTML+='<div><a href="https://github.com/djytw?tab=repositories">我的其他项目...</a></div>';
 }
@@ -154,8 +172,14 @@ function util_github_getrepos() {
 		oReq.responseType = "json";
 		oReq.onload =  function (err) {
 			var e = oReq.response;
-			util_github_reposrender(e);
-			Cookies.set("githubrepo",JSON.stringify(e));
+			var ret = [];
+			for (var i = 0; i < e.length; i++) {
+				ret.push({url: e[i].html_url, name:e[i].name, des:e[i].description});
+				if (i >= 4) break;
+			}
+			var exp = new Date(new Date().getTime() + 5 * 60 * 1000);
+			Cookies.set("githubrepo",JSON.stringify(ret),{expires: exp});
+			util_github_reposrender(ret);
 		};
 		oReq.send(null);
 	} else {
